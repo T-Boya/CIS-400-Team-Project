334ea81c43ebd1a8f3ed7d742ba015db598b8cc3
Tinashe Osewe <t.osewe1@gmail.com>
2021-05-17 23:28:20 +0300

    commented live_data_analyzer

--- prediction/live_data_analyzer.py
+++ prediction/live_data_analyzer.py
@@ -18,12 +18,9 @@ import sys
 
 
 def oauth_login():
-    # XXX: Go to http://twitter.com/apps/new to create an app and get values
-    # for these credentials that you'll need to provide in place of these
-    # empty string values that are defined as placeholders.
-    # See https://developer.twitter.com/en/docs/basics/authentication/overview/oauth
-    # for more information on Twitter's OAuth implementation.
-    
+    '''
+    Creates logged-in twitter API oauth object
+    '''    
     CONSUMER_KEY = 'KmtQre6nXaxlhrov6b98zoo3W'
     CONSUMER_SECRET = 'YQBO45L8IkuOeHJSwWtchOlVjzJBDkQwOZoZMgLzg4TN0NV8Ft'
     OAUTH_TOKEN = '1366839526208004101-Hjs0iSxyWGzbrLoc3BFU6KDF2oX4DE'
@@ -42,57 +39,55 @@ def Current_Tweets_Sentiment(listOfTerms, numberOfTweet):
     Outputs the number of democratic and republican tweets
     '''
 
+    # Create some variables to store data
     tweet_Max = numberOfTweet
     tweet_Counter = 0
     list_Of_Tweets = []
     list_Of_Weights = []
 
-    # Returns an instance of twitter.Twitter
+    # Set up stream
     twitter_api = oauth_login()
-    # Reference the self.auth parameter
     twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
-
     print('Filtering the public timeline for track = {0}'.format(listOfTerms), file=sys.stderr)
     sys.stderr.flush()
 
+    # Attempt to collect 100 politically relevant tweets from stream
     try:
         stream = twitter_stream.statuses.filter(track=listOfTerms)
         for tweet in stream:
-            max_errors = 10
             try:
-
-                #Making a list of all tweets and weights
-                if 'limit' in tweet:
-                    print(tweet)
-                    print('sleeping for 2 mins')
-                    time.sleep(60 * 2 + 5)
-                    continue
-
+                # Create list of tweet bodies and list of weights using community detection algorithm
                 list_Of_Tweets.append(tweet['text'])
                 list_Of_Weights.append(max(1, int(round(int(tweet['retweet_count']) * 0.4 + int(tweet['favorite_count']) * 0.2))))
 
-                #get a number of tweet in stream
+                # Stop collecting tweets once desired number has been reached
                 tweet_Counter += 1
                 if tweet_Counter == tweet_Max:
                     break
 
+            # Handle error
             except:
                 return True, 0, 0
+
+    # Handle error if rate limiting is occurring
     except:
         return True, 0, 0
 
+    # Run sentiment analysis on tweets
     sys.stdout.flush()
-
-    #New Sentiment analysis on listOfTweets
     classifier, vect = create_classifier()
     tot = len(list_Of_Tweets)
     i = 0
     correct_dem = 0
     correct_rep = 0
     ef = classify_tweet(classifier, vect, list_Of_Tweets)
+
+    # Print statements to show progress in console
     print(ef)
     print(str(len(list_Of_Tweets)), "tweets")
     print(str(len(ef)), "analyzed")
+
+    # Prepare data for webpage
     for i in range(len(ef)):
         if ef[i] == "Democrat":
                 correct_dem += list_Of_Weights[i]
