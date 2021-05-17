5cdc1a34351d3f2f144616134474f7384bbc3371
DanielPae <danielpae104@gmail.com>
2021-05-17 16:42:08 -0400

    added comments

--- past_tweet_analyzer.py
+++ past_tweet_analyzer.py
@@ -1,12 +1,13 @@
 import time
 import sentiment_analysis
-import twitter
 import requests
 from requests_oauthlib import OAuth1
 
-
+# given a starting month, day, year, classifier, and a file to look through
+# pull out tweet ids, classifiy about 1000 tweets for that day, write the number of rep and dem tweets to a file
+# then move on to the next day and continue until we run out of data to look thorugh
 def analyize_tweets(month, day, year, au, classifier, wf, wfile):
-    if month < 10:
+    if month < 10: # this stuff just normalizes the month and day strings so we can use them to find a file in the directory
         smonth = "0" + str(month)
     else:
         smonth = str(month)
@@ -15,39 +16,40 @@ def analyize_tweets(month, day, year, au, classifier, wf, wfile):
     else:
         sday = str(day)
     with open("us-pres-elections-2020/20" + str(year) + "-" + smonth + "/us-presidential-tweet-id-20"
-         + str(year) + "-" + smonth + "-" + sday + "-00.txt", "r", newline='') as twFile:
+         + str(year) + "-" + smonth + "-" + sday + "-00.txt", "r", newline='') as twFile: # put the data together and use it to find the file we are looking for
         num_ids = 0
-        calls_for_this_file = 0
-        ids = ""
-        rep = 0
+        calls_for_this_file = 0 # if we make more than 20 api calls for one day we just move on for rate limiting reasons
+        ids = "" # we have to send the ids in strings so we start that here
+        rep = 0 # how many reps and dems we have found so far
         dem = 0
-        for id in twFile:
-            if num_ids < 99:
+        for id in twFile: # for each id in the file
+            if num_ids < 99: # if we have less than 99 ids then just keep looking for ids
                 ids = ids + id.strip() + ","
                 num_ids += 1
             else:
-                if num_ids < 100:
+                if num_ids < 100: # after 99 ids add the last one
                     ids = ids + id.strip()
                     num_ids += 1
-                try:
+                try: # then send the 100 ids to twitter to get back the full text
                     s = requests.get("https://api.twitter.com/1.1/statuses/lookup.json?", params={'id': ids}, auth=au)
                     if s.status_code != 200:
-                        if s.json()["errors"][0]["code"] == "88":
+                        if s.json()["errors"][0]["code"] == 88: # rate limit catching
                             raise RuntimeError("Hit Rate Limit")
                         else:
                             print("Bad Response from twitter. Exiting program")
+                            print(s.json())
                             twFile.close()
                             return 0
-                    for t in s.json():
-                        guess = sentiment_analysis.classify_tweet(classifier, wf, t["text"])
-                        if guess == "Democrat":
-                            dem += 1
-                        else:
-                            rep += 1
+                    tweets = []
+                    for t in s.json(): # put all the tweets into one list so we can classify them
+                        tweets.append(t["text"])
+                    dem_t, rep_t = sentiment_analysis.classify_tweets_count(classifier, wf, tweets) # classify them
+                    dem += dem_t # add the number of dems and reps to the totals for the day
+                    rep += rep_t
                     print("Dem: " + str(dem) + ", Rep: " + str(rep))
-                    ids = ""
+                    ids = "" # reset the id string and number of id var
                     num_ids = 0
-                    if calls_for_this_file < 20 and dem + rep <= 1000:
+                    if calls_for_this_file < 20 and dem + rep <= 1000: # if we have found at least 1000 total tweets or we have made 20 api calls then we write this day to our output file and move on
                         calls_for_this_file += 1
                     else:
                         wfile.write(str(month) + "/" + str(day) + "/" + str(year) + " " + str(rep) + " " + str(dem) + "\n")
@@ -61,25 +63,18 @@ def analyize_tweets(month, day, year, au, classifier, wf, wfile):
 
 
 if __name__ == "__main__":
-    """
-    myAuth = twitter.oauth.OAuth(consumer_key='cvUnT3e5uMm7URiYZscsED8DF',  # just auth stuff
-                                 consumer_secret='IfFSYmujlz2F3HlKQk8mZ6sgZc10646pniF2Utvd8lEx4l1Ckg',
-                                 token='980873561886937088-jgzxBfvmibqClm8twVLzqFiUBHvK4ua',
-                                 token_secret='QNTPfO6UKO92t6ECZb2o8L9Dig2uYtSn2OSi5ApOuqQSC')
-    tw = twitter.Twitter(auth=myAuth)
-    """
     au = OAuth1('cvUnT3e5uMm7URiYZscsED8DF', 'IfFSYmujlz2F3HlKQk8mZ6sgZc10646pniF2Utvd8lEx4l1Ckg', "980873561886937088-jgzxBfvmibqClm8twVLzqFiUBHvK4ua", 'QNTPfO6UKO92t6ECZb2o8L9Dig2uYtSn2OSi5ApOuqQSC')
     classifier, wf = sentiment_analysis.create_classifier()
     print("Classifier created")
     with open("past_tweet_analysis.txt", "a") as output:
-        year = 21
+        year = 20
         day = 1
         month = 3
         while year != 21 or month != 5:
             worked = analyize_tweets(month, day, year, au, classifier, wf, output)
             if worked != 0:
                 print("Analyized " + str(month) + "/" + str(day) + "/" + str(year))
-                if day == 31 or (day == 30 and (month == 4 or month == 6 or month == 9 or month == 11)) or (month == 2 and day == 29 and (year%4 != 0)) or (month == 2 and day == 28 and (year%4 != 0)):
+                if day == 31 or (day == 30 and (month == 4 or month == 6 or month == 9 or month == 11)) or (month == 2 and day == 29 and (year%4 == 0)) or (month == 2 and day == 28 and (year%4 != 0)):
                     if month == 12:
                         year += 1
                         month = 1
