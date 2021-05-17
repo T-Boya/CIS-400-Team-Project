17c39f5256a082a2b20a9f536f30d85d4998a73d
Tinashe Osewe <t.osewe1@gmail.com>
2021-05-17 18:45:30 +0300

    reactivated livesearch view (changed from using fixed data to twitter data)

--- prediction/views.py
+++ prediction/views.py
@@ -55,12 +55,12 @@ def index(request):
 def load_live(request):
     democrat_kwarg_list = 'biden, democratic, democrat' # API is case insensitive
     republican_kwarg_list = 'trump, republican, gop'
-    bipartisan_kwarg_list = democrat_kwarg_list + ', ' + republican_kwarg_list
-    # democrats, dem_tweets, republicans, rep_tweets = Current_Tweets_Sentiment(bipartisan_kwarg_list, 100)
-    democrats = 150
-    dem_tweets = []
-    republicans = 50
-    rep_tweets = []
+    # bipartisan_kwarg_list = democrat_kwarg_list + ', ' + republican_kwarg_list
+    democrats, dem_tweets, republicans, rep_tweets = Current_Tweets_Sentiment(bipartisan_kwarg_list, 100)
+    # democrats = 150
+    # dem_tweets = []
+    # republicans = 50
+    # rep_tweets = []
     print('in the view now')
     print("Democratic: ", democrats, " Republican: ", republicans)
     winner = "DEMOCRATS" if democrats > republicans else "REPUBLICANS"
